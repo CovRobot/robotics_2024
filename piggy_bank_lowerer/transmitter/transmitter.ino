6c690e9892061478ca303e2b4fb9620e524d5a44
Gabe Righter <gaberighter@gmail.com>
2024-10-22 12:21:08 -0400

    did some work on the transmitter.ino file to try and get it to work correctly. THe code compiles but has not been actually tested because I'm at home and also because I haven't done the code for the reciever yet.

--- piggy_bank_lowerer/transmitter/transmitter.ino
+++ piggy_bank_lowerer/transmitter/transmitter.ino
@@ -1,3 +1,4 @@
+#include <SPI.h>
 #include <RF24.h>
 
 // Replace with your desired RF24 library pins
@@ -8,33 +9,47 @@
 RF24 radio(CE_PIN, CSN_PIN);
 
 // Address for the receiver
-const uint8_t address[6] = {0x12, 0x13, 0x14, 0x15, 0x16, 0x17};
+uint8_t address[][6] = {"trans", "piggy"};
+
+//defined content options for the messages from this system to the pulley system
+#define FORWARD 1
+#define BACK 0
 
 // Function to send a control message
-void sendControlMessage(bool direction) {
-  radio.stopListening();
-  uint8_t data = direction ? 1 : 0; // 1 for forward, 0 for backward
-  radio.write(&data, sizeof(data));
-  radio.startListening();
+void sendControlMessage(byte message) {
+  // Send the message using the write function. Arguments for the write function are
+  // a pointer to the message and the length of the message
+  radio.write(&message, sizeof(message));
 }
 
 void setup() {
-  // Initialize the RF24 module
-  radio.begin();
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.setDataRate(RF24_250KBPS);
-  radio.openWritingPipe(address);
+  // Begin serial channel for debugging
+  Serial.begin(9600);
+
+  // Check to make sure that the rf24 module was initialized succesfully
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware not responding!"));
+    while (1) {} // hold program in infinite loop to prevent subsequent errors
+  }
+
+  // prepare to send messages
+  radio.openWritingPipe(address[1]);
 }
 
 void loop() {
+  // Send messages indicating direction to the pulley system. The system will
+  // receive those messages and move for like 200 millis when it gets a direction.
+  // Otherwise it won't do anything.
+
+
   // Replace with your desired input method (e.g., buttons, joystick)
   bool forward = digitalRead(A0); // Example: Read analog input for forward direction
   bool backward = digitalRead(A1); // Example: Read analog input for backward direction
 
   // Send control message based on input
   if (forward) {
-    sendControlMessage(true);
+    sendControlMessage(FORWARD);
   } else if (backward) {
-    sendControlMessage(false);
+    sendControlMessage(BACK);
   }
 }
